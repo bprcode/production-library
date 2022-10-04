618c7637b9cff657255f0c1dfcdfbd02a3751fd7
bprcode <bprcode@gmail.com>
2022-10-04 01:47:17 -0700

    Proof of concept for hbs layout

--- app.js
+++ app.js
@@ -6,19 +6,33 @@ const path = require('node:path')
 require('@bprcode/handy')
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+hbs.registerPartials(path.join(__dirname, '/views/partials'))
 
 const homeRouter = require('./routes/home.js')
 
+
 app
+    .use((req, res, next) => {
+        log(req.method + ': ' + req.originalUrl, dim)
+        next()
+    })
+
     .use('/', homeRouter)
+
     .use(express.static(path.join(__dirname, 'public')))
-    // Fallthrough handler: route not found
+
     .use((req, res, next) => {
         res.status(404)
         throw new Error('File not found.')
     })
+
     .use((err, req, res, next) => {
-        res.send('An error occurred: ' + err.message)
+        res.render('error.hbs', {
+            title: 'Error encountered',
+            status_code: res.statusCode,
+            error_message: err.message
+        })
     })
 
 const server = app.listen(process.env.PORT || 2666, () => {
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const router = express.Router()
+require('@bprcode/handy')
 
 router
     .get('/', (req, res, next) => {
-        res.render('home.hbs',
-            {   title: 'Welcome to the Home Page',
-                page: 'Sample Rendered Output'
+        res.render('home.hbs', {
+            title: 'Welcome to the Home Page',
+            page: 'Sample Rendered Output'
             })
+    })
+    .get('/moo/:count', (req, res, next) => {
+        log(`Rendering...`, green)
+        res.render('moo.hbs', {
+            partial_header: 'head',
+                 top_face: '🐲',
+            partial_footer: 'foot',
+            partial_name: Math.random() < 0.5 ? 'foo' : 'bar',
+            title: req.query.top || 'Quack Quack',
+            animals: Array.from({ length: req.params.count }, _ => moo()),
+            baz: req.query.baz || 111.222
+        })
     })
 
 module.exports = router
