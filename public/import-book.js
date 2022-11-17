f7bc9dd1da96a3c9af915f2e1d35f73f3be23a46
bprcode <bprcode@gmail.com>
2022-11-16 21:44:25 -0800

    Begin separating import code to module

--- public/import-book.js
+++ public/import-book.js
@@ -1,11 +1,16 @@
-const openLibraryAddress = `https://openlibrary.org/search.json`
-const bookApiAddress = `https://openlibrary.org/search/books.json`
-const bookDetailAddress = `https://openlibrary.org/books/`
-const workRootAddress = `https://openlibrary.org`
+import * as common from "./import-common.js"
+
+const openLibraryAddress =  `https://openlibrary.org/search.json`
+const bookApiAddress =      `https://openlibrary.org/search/books.json`
+const bookDetailAddress =   `https://openlibrary.org/books/`
+const authorDetailAddress = `https://openlibrary.org/authors/`
+const workRootAddress =     `https://openlibrary.org`
 
 // Convenience shorthands
 const log = console.log.bind(console)
 const el = document.getElementById.bind(document)
+
+log('imported k=', common.k) 
 
 const listTemplate = el('list-template')
 const renderList = Handlebars.compile(listTemplate.innerHTML.trim())
@@ -42,20 +47,74 @@ async function handleDescriptionToggle (event) {
 }
 
 async function revealModal (event) {
-    log('Modal template is:')
-    log(revealModal.template)
+    // Todo: compile appropriate requests into a parallel Promise.all
 
-    revealModal.template ??=
-        await
-            (await fetch('/templates/book_form_body.hbs'))
-        .text()
-    revealModal.renderTemplate ??= Handlebars.compile(revealModal.template)
+    // Initialize templates when first needed
+    revealModal.bookTemplate ??= 
+        await (await fetch('/templates/book_form_body.hbs')).text()
 
-    log('relatedTarget:')
-    log(event.relatedTarget)
-    
+    revealModal.renderBookForm ??=
+        Handlebars.compile(revealModal.bookTemplate)
+
+    revealModal.authorTemplate ??=
+        await (await fetch('/templates/author_form_body.hbs')).text()
+
+    revealModal.renderAuthorForm ??=
+        Handlebars.compile(revealModal.authorTemplate)
+
+
+    const dataset = event.relatedTarget.dataset
+
+    const modalBook = el('modal-body-book')
+    const modalAuthor = el('modal-body-author')
+    modalBook.innerHTML = 'Loading...'
+
+    try {
+        const work = await (await fetch(
+            workRootAddress + dataset.key + '.json'
+            )).json()
+
+        let coverEd = {}
+        if (dataset.editionKey) {
+            coverEd = await (await fetch(
+                bookDetailAddress + dataset.editionKey + '.json'
+            )).json()
+        }
+
+        let author = {}
+        if (dataset.firstAuthor) {
+            author = await (await fetch(
+                authorDetailAddress + dataset.firstAuthor + '.json'
+            )).json()
+        }
+
+        log('dataset:', dataset)
+        log('work object:', work)
+        log('cover edition object:', coverEd)
+        log('first author:', author)
+
+        modalBook.innerHTML = revealModal.renderBookForm({
+            populate: {
+                title: work.title,
+                summary: work.description,
+                // Suggested ISBNs, in order of preference:
+                isbn: coverEd.isbn_10 || coverEd.isbn_13
+                        || dataset.firstIsbn || ''
+            },
+            omit_author: true
+        })
+        modalAuthor.innerHTML = revealModal.renderAuthorForm({
+            author: {last_name: 'Placeholder author'},
+            collapse: true
+        })
+
+    } catch (e) {
+        log('error: ', e)
+        modalBook.innerHTML = `Record not currently available.`
+    }
+
     return
-
+/*
     const authorKey = event.relatedTarget.dataset.key
     const modalBody = event.target.querySelector('#modal-body-id')
     let response
@@ -87,6 +146,7 @@ async function revealModal (event) {
     }
 
     modalBody.innerHTML = revealModal.renderTemplate({ author, trouble })
+    */
 }
 
 el('input-modal').addEventListener('show.bs.modal', revealModal)
@@ -101,17 +161,23 @@ el('search-button').addEventListener('click', async event => {
         title: query,
         limit: 20,
         page: 1,
-        fields: [
+        /*fields: [
             'key', 'title', 'author_name', 'first_publish_year', 'subject',
              'author_key',
              'cover_edition_key',
         ]
+        */
         // possibly take cover_edition_key by preference?
         // lots of isbns available, suggest taking one from the particular
         // edition preferred, not just the first one in the isbn arary
 
         // ... seems like we need description from the work key,
         // perhaps ISBN preferentially from the cover edition?
+        // ... actually, the existence of a cover edition ISBN seems
+        // uncertain. Ex: Edith Hamilton's "Mythology" has 72 ISBNs,
+        // but the cover edition has none.
+        // Other Cover Editions do have ISBNs, maybe multiple, by names
+        // such as isbn_10 or isbn_13, which may be populated, or not
     })
 
     let queryUrl = new URL(openLibraryAddress)
@@ -127,6 +193,8 @@ el('search-button').addEventListener('click', async event => {
 
     const response = await fetch(queryUrl)
     const json = await response.json()
+
+    log(json)
 
     for (const e of document.querySelectorAll('.description')) {
         e.removeEventListener('toggle', handleDescriptionToggle)
---
+++ public/import-common.js
@@ -0,0 +1,9 @@
+console.log('Common import code loaded.')
+
+export const openLibraryAddress =  `https://openlibrary.org/search.json`
+export const bookApiAddress =      `https://openlibrary.org/search/books.json`
+export const bookDetailAddress =   `https://openlibrary.org/books/`
+export const authorDetailAddress = `https://openlibrary.org/authors/`
+export const workRootAddress =     `https://openlibrary.org`
+
+
