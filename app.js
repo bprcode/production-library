26dac3db29be288fc5f5393f6eb5361a8cfecc11
bprcode <bprcode@gmail.com>
2022-11-21 14:12:17 -0800

    Remove bootstrap CSS source map

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ const server = app.listen(process.env.PORT || 2666, () => {
     log('Would be nice: encapsulate book creation with its genres as a transaction.')
     log('There is a 2px doubled border on book_detail availability. Low-priority but debug.')
     log('Kinda need pagination everywhere.')
-    log('Do not want to include bootstrap css map, but console errors look bad -- maybe serve 0kb file later?')
     log('To fix: failures on import when no genres are recorded in database')
+    log('To fix: better presentation on 0-result queries for imports')
 })
