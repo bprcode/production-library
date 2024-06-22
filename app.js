d51a10c7c388631db40f9eb9b0dab4fe0a8d07f3
bprcode <bprcode@gmail.com>
2024-06-22 12:40:46 -0700

    Remove crash test routes

--- app.js
+++ app.js
@@ -74,9 +74,6 @@ app
     .get('/', (req, res) => { res.redirect('/catalog') })
     .use('/catalog', catalogRouter)
     .use('/reset', resetRouter)
-    // DEBUG:
-    .get('/fail', () => { const x = 1; x++; })
-
 
     .use(express.static(path.join(__dirname, 'public')))
 
@@ -86,13 +83,6 @@ app
     })
 
     .use((err, req, res, next) => {
-        // DEBUG:
-        if (req.originalUrl === '/crash') {
-            log('testing crash response')
-            const x = 1;
-            x++;
-        }
-
         res.render('error.hbs', {
             title: 'Error Encountered',
             status_code: res.statusCode,
